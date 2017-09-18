edb1a3a0867d2e482db7e6597969f1374dd323e1
Andriy Kashcha <anvaka@gmail.com>
2017-09-18 00:21:07 -0700

    Added a few optimization
    This reduced GC pressure, and improved performance by 20%

--- a-star/nba.js
+++ a-star/nba.js
@@ -1,29 +1,68 @@
 module.exports = nba;
 
 var NodeHeap = require('./NodeHeap');
-//var NodeSearchState = require('./NodeSearchState');
 var heuristics = require('./heuristics');
 var defaultSettings = require('./defaultSettings.js');
 
-var NodeSearchState = function NodeSearchState(node) {
+function makeSearchStatePool() {
+  var currentInCache = 0;
+  var nodeCache = [];
+
+  return {
+    createNewState: createNewState,
+    reset: reset
+  };
+
+  function reset() {
+    currentInCache = 0;
+  }
+
+  function createNewState(node) {
+    var cached = nodeCache[currentInCache];
+    if (cached) {
+      cached.node = node;
+
+      // How we came to this node?
+      cached.p1 = null;
+      cached.p2 = null;
+
+      cached.closed = false;
+
+      cached.g1 = Number.POSITIVE_INFINITY;
+      cached.g2 = Number.POSITIVE_INFINITY;
+      cached.f1 = Number.POSITIVE_INFINITY;
+      cached.f2 = Number.POSITIVE_INFINITY;
+
+      // used to reconstruct heap when fScore is updated.
+      cached.h1 = -1;
+      cached.h2 = -1;
+    } else {
+      cached = new NBASearchState(node);
+      nodeCache[currentInCache] = cached;
+    }
+    currentInCache++;
+    return cached;
+  }
+}
+
+function NBASearchState(node) {
   this.node = node;
 
   // How we came to this node?
   this.p1 = null;
   this.p2 = null;
 
-  this.inMiddle = true;
+  this.closed = false;
 
   this.g1 = Number.POSITIVE_INFINITY;
   this.g2 = Number.POSITIVE_INFINITY;
-  // the f(n) = g(n) + h(n) value
   this.f1 = Number.POSITIVE_INFINITY;
   this.f2 = Number.POSITIVE_INFINITY;
 
   // used to reconstruct heap when fScore is updated.
   this.h1 = -1;
   this.h2 = -1;
-};
+}
 
 var NO_PATH = defaultSettings.NO_PATH;
 
@@ -39,6 +78,7 @@ function nba(graph, options) {
 
   var distance = options.distance;
   if (!distance) distance = defaultSettings.distance;
+  var pool = makeSearchStatePool();
 
   return {
     /**
@@ -55,7 +95,9 @@ function nba(graph, options) {
     var to = graph.getNode(toId);
     if (!to) throw new Error('toId is not defined in this graph: ' + toId);
 
-    // Maps nodeId to NodeSearchState.
+    pool.reset();
+
+    // Maps nodeId to NBASearchState.
     var nodeState = new Map();
 
     // the nodes that we still need to evaluate
@@ -71,32 +113,27 @@ function nba(graph, options) {
     var minNode;
     var lMin = Number.POSITIVE_INFINITY;
 
-    var startNode = new NodeSearchState(from);
+    var startNode = pool.createNewState(from);
     nodeState.set(fromId, startNode); 
     startNode.g1 = 0;
     var f1 = heuristic(from, to);
     startNode.f1 = f1;
     open1Set.push(startNode);
 
-    var endNode = new NodeSearchState(to);
+    var endNode = pool.createNewState(to);
     nodeState.set(toId, endNode);
     endNode.g2 = 0;
-    var f2 = heuristic(to, from);
+    var f2 = f1; // they should agree originally
     endNode.f2 = f2;
     open2Set.push(endNode)
 
-    var finished = false;
-    var f1Finsihed = false;
-    var f2Finished = false;
     var cameFrom;
-    while (!finished) {
+    while (open2Set.length && open1Set.length) {
       if (open1Set.length < open2Set.length) {
         forwardSearch();
       } else {
         reverseSearch();
       }
-
-      finished = f1Finsihed || f2Finished;
     }
 
     // If we got here, then there is no path.
@@ -105,46 +142,45 @@ function nba(graph, options) {
 
     function forwardSearch() {
       cameFrom = open1Set.pop();
-      if (cameFrom.inMiddle) {
-        cameFrom.inMiddle = false;
+      if (cameFrom.closed) {
+        return;
+      }
 
-        if (cameFrom.f1 < lMin && (cameFrom.g1 + f2 - heuristic(from, cameFrom.node))) {
-          graph.forEachLinkedNode(cameFrom.node.id, visitN1, options.oriented) // todo - needs to reverse correctly
-        }
+      cameFrom.closed = true;
+
+      if (cameFrom.f1 < lMin && (cameFrom.g1 + f2 - heuristic(from, cameFrom.node)) < lMin) {
+        graph.forEachLinkedNode(cameFrom.node.id, visitN1, options.oriented) // todo - needs to reverse correctly
       }
 
       if (open1Set.length > 0) {
         f1 = open1Set.peek().f1;
-      } else {
-        f1Finsihed = true;
-      }
+      } 
     }
 
     function reverseSearch() {
       cameFrom = open2Set.pop();
-      if (cameFrom.inMiddle) {
-        cameFrom.inMiddle = false;
+      if (cameFrom.closed) {
+        return;
+      }
+      cameFrom.closed = true;
 
-        if (cameFrom.f2 < lMin && (cameFrom.g2 + f1 - heuristic(cameFrom.node, to))) {
-          graph.forEachLinkedNode(cameFrom.node.id, visitN2, options.oriented) // todo - needs to reverse correctly
-        }
+      if (cameFrom.f2 < lMin && (cameFrom.g2 + f1 - heuristic(cameFrom.node, to)) < lMin) {
+        graph.forEachLinkedNode(cameFrom.node.id, visitN2, options.oriented) // todo - needs to reverse correctly
       }
 
       if (open2Set.length > 0) {
         f2 = open2Set.peek().f2;
-      } else {
-        f2Finished = true;
       }
     }
 
     function visitN1(otherNode, link) {
       var otherSearchState = nodeState.get(otherNode.id);
       if (!otherSearchState) {
-        otherSearchState = new NodeSearchState(otherNode);
+        otherSearchState = pool.createNewState(otherNode);
         nodeState.set(otherNode.id, otherSearchState);
       }
 
-      if (!otherSearchState.inMiddle) return;
+      if (otherSearchState.closed) return;
 
       var tentativeDistance = cameFrom.g1 + distance(cameFrom.node, otherNode, link);
 
@@ -168,11 +204,11 @@ function nba(graph, options) {
     function visitN2(otherNode, link) {
       var otherSearchState = nodeState.get(otherNode.id);
       if (!otherSearchState) {
-        otherSearchState = new NodeSearchState(otherNode);
+        otherSearchState = pool.createNewState(otherNode);
         nodeState.set(otherNode.id, otherSearchState);
       }
 
-      if (!otherSearchState.inMiddle) return;
+      if (otherSearchState.closed) return;
 
       var tentativeDistance = cameFrom.g2 + distance(cameFrom.node, otherNode, link);
 
